1e6adfe5e841bc5b82e574c03e1fcac9dc4ed1fc
Sam Joseph <tansaku@gmail.com>
2013-01-10 11:22:16 +0000

    getting a basic chat html page set up
    bit of jquery - should work next on tests and persistence

--- faq.js
+++ faq.js
@@ -1,8 +1,26 @@
-function query(sentence){
-	// check for sentence word by word in list (hashtable)
-	var words = sentence.split(" ");
-	var lookup = "";
-	var prepend = "";
+$(document).ready(function () {
 
-	return "OK";
-};
+    function query(sentence) {
+        // check for sentence word by word in list (hashtable)
+        var words = sentence.split(" ");
+        var lookup = "";
+        var prepend = "";
+    
+        return "OK";
+    }
+    
+    function updateHistory(sentence) {
+        $("div#history").append(sentence);
+    }
+    
+    function handleChat(sentence) {
+        updateHistory(sentence + "<br/>");
+        updateHistory(query(sentence) + "<br/>");
+         
+        return false;
+    }
+    
+    $("form#chat").submit(function () {
+        return handleChat($("input#sentence").val());
+    });
+});
