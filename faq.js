a3f07d060ad3b4bf62f461c7d68e2d9ce09ccc9e
Sam Joseph <tansaku@gmail.com>
2013-01-17 10:37:15 +0000

    adding some Xregexp goodness
    not sure what the policy should be regarding other js libraries - check
    them in? I guess that's simpler ...

--- faq.js
+++ faq.js
@@ -5,8 +5,33 @@ $(document).ready(function () {
         var words = sentence.split(" ");
         var lookup = "";
         var prepend = "";
-    
-        return "OK";
+        
+        // things to do here
+        // 1. grab first response from google on query on what user said
+        // --- need way to grab URL of request from Google
+        // 2. process assertions, e.g. 'there is a course called Mobile Design & Programming'
+        // --- that relies on some form of regex and also persistence
+        // 3. process queries, e.g. what is the start date of Mobile Design & Programming
+        // --- want to be able to check for bigrams/trigrams having removed stopwords
+        // 4. want to log transcript to some persistence store
+        // 5. could add eliza/twss code?
+        
+        // Using named capture and flag x (free-spacing and line comments)
+        var assert = XRegExp('(?<assert>  There(\\si|\')s\\sa ) \\s?  # assert  \n' +
+                             '(?<object> [^\\s]* ) \\s?  # object \n' +
+                             '(?<called> called ) \\s?  # called \n' +
+                             '(?<name>   .* )     # name     ', 'x');
+        // so perhaps we could just create a json structure to reflect the assertion ...
+        // I guess ultimately we really want that flexible parse structure to handle
+        // a) Mobile is a new course
+        // b) I heard that there's a new course called Mobile
+        // c) Have you signed up for that new Mobile course?
+        var match = XRegExp.exec(sentence, assert)
+        // want to check is match is undefined or not ...
+        var response = "OK";
+        if(match !== null)
+            response = match.name + ' is a ' +match.object;
+        return response;
     }
     
     function updateHistory(sentence) {
