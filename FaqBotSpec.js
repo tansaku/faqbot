d41d5207529a8fb2aa68c9be5c42f100cff3581e
Sam Joseph <tansaku@gmail.com>
2013-03-20 19:46:31 +0000

    oh no forgot that

---
+++ FaqBotSpec.js
@@ -0,0 +1,33 @@
+describe("FaqBot", function() {
+  var sentences = [];
+  var answers = [];
+
+  it("should respond as expected ", function() {
+    expect(query("There is a game engine Unreal Engine")).toEqual("what was that?");
+  });
+
+  
+  sentences[0] = "There is a game engine called Unreal Engine";
+  answers[0] = "Unreal Engine is a game engine";
+  sentences[1] = "There is a horse called Matilda";
+  answers[1] = "Matilda is a horse";
+  sentences[2] = "There is a course called ML";
+  answers[2] = "ML is a course";
+
+  for (var i in sentences){
+    it( "should respond to \""+sentences[i] + "\" with --> \"" + answers[i]+ "\"", function() {  
+      expect(query(sentences[i])).toEqual(answers[i]);
+    });
+  }
+
+  it("should respond from database when asked about a one word item", function() {
+    expect(query("There is a course called ML")).toEqual("ML is a course");
+    expect(query("What do you know about ML")).toEqual("I know that ML is a course");
+  });
+
+  it("should respond from database when asked about a two word item", function() {
+    expect(query("There is a game engine called Unreal Engine")).toEqual("Unreal Engine is a game engine");
+    expect(query("What do you know about Unreal Engine")).toEqual("I know that Unreal Engine is a game engine");
+  });
+
+});
